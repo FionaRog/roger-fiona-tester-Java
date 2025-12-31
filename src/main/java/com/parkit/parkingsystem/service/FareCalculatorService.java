8cadee68664e951a548f7bdeddf7f15181598ed6
Fiona <fiona.rog.fr@gmail.com>
2025-12-31 09:27:17 +0100

    Bug correction of the calculation method for the variable duration

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,6 +3,9 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
@@ -10,11 +13,12 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        long inTime = ticket.getInTime().getTime();
+        long outTime = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+                long durationInMillis = outTime - inTime;
+                double duration = durationInMillis / (1000.0 * 60 * 60);
+                duration = Math.round(duration * 100.0) / 100.0;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
